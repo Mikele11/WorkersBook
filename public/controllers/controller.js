d3f4b7179ce93e448872fa8a46b5ff67950c32a5
Your Name <mihailo_rlyash@mail.ru>
2017-02-15 14:42:56 +0200

    first commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,78 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("I am in controller");
+    $scope.filteredTodos = []
+   ,$scope.currentPage = 1
+   ,$scope.numPerPage = 2
+   ,$scope.maxSize = 5;
+   
+var refresh = function() {
+  $http.get('/bookday').success(function(response) {
+    console.log("Geter");
+    $scope.bookday = response;
+	console.log($scope.bookday);
+	$scope.len=$scope.bookday.length;
+	$scope.page=Math.ceil($scope.len/$scope.numPerPage);
+	console.log($scope.page);
+    $scope.contact = "";
+  });
+};
+refresh();
+//панинация
+  $scope.numPages = function () {
+	$http.get('/bookday').success(function(response) {
+    $scope.bookday = response;
+    $scope.numPages= Math.ceil($scope.bookday.length / $scope.numPerPage);
+	console.log($scope.bookday.length+'+++++++');
+  });  
+  };
+  
+  $scope.$watch('currentPage + numPerPage', function() {
+	$http.get('/bookday').success(function(response) {
+	var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+    var end = begin + $scope.numPerPage; 
+    $scope.filteredTodos = $scope.bookday.slice(begin, end);
+	}); 
+  });  
+//конец пагинации
+$scope.addContact = function() {
+  console.log($scope.contact);
+  //---------------
+    d0 = new Date($scope.contact.dataB);
+	d1 = new Date();
+	dt = (d1.getTime() - d0.getTime()) / (1000*60*60*24);
+	dtRound = Math.round(dt);
+	$scope.contact.registr=d1;
+	//--
+  $http.post('/bookday', $scope.contact).success(function(response) {
+    console.log(response);
+    refresh();
+  });
+};
+
+$scope.remove = function(id) {
+  console.log(id);
+  $http.delete('/bookday/' + id).success(function(response) {
+    refresh();
+  });
+};
+
+$scope.edit = function(id) {
+  console.log(id);
+  $http.get('/bookday/' + id).success(function(response) {
+    $scope.contact = response;
+  });
+};  
+
+$scope.update = function() {
+  console.log($scope.contact._id);
+  $http.put('/bookday/' + $scope.contact._id, $scope.contact).success(function(response) {
+    refresh();
+  })
+};
+
+$scope.deselect = function() {
+  $scope.contact = "";
+}
+
+}]);﻿
---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('bookday', ['bookday']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/bookday', function (req, res) {
+  console.log('All right');
+
+  db.bookday.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  })
+});
+
+app.post('/bookday', function (req, res) {
+  console.log(req.body);
+  db.bookday.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/bookday/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.bookday.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/bookday/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.bookday.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/bookday/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.bookday.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, gender: req.body.gender, phone: req.body.phone}},//change avtomatic change
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
